f816925e342e0ab1d1d106f4deac57ee3bf38020
Hugo <hfernan98@gmail.com>
2025-11-11 18:21:50 -0500

    Fin Seccion 9 - modify config variable

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CNN);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Base de datos online');
   } catch (error) {
     console.error('❌ Error al conectar con MongoDB:', error.message);
